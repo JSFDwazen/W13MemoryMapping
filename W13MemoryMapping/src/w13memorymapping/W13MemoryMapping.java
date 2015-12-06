9872a63d38e28ebdf531969de754ffcdff9c5d25
jsf3 <jsf3@jsf3-virtual-machine>
2015-12-06 19:58:50 +0100

    added FileMapped read function

--- W13MemoryMapping/src/w13memorymapping/W13MemoryMapping.java
+++ W13MemoryMapping/src/w13memorymapping/W13MemoryMapping.java
@@ -5,23 +5,19 @@
  */
 package w13memorymapping;
 
-import java.io.BufferedOutputStream;
-import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.io.ObjectOutputStream;
 import java.io.RandomAccessFile;
+import java.nio.CharBuffer;
 import java.nio.MappedByteBuffer;
 import java.nio.channels.FileChannel;
+import java.nio.charset.Charset;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Observable;
 import java.util.Observer;
 import java.util.Scanner;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  *
@@ -37,10 +33,12 @@ public class W13MemoryMapping implements Observer {
         W13MemoryMapping w13MemoryMapping = new W13MemoryMapping();
     }
 
-    KochFractal koch = new KochFractal();
+    private KochFractal koch;
     private File fileMapped;
+    private int level;
 
     public W13MemoryMapping() throws IOException {
+        this.koch = new KochFractal();
         this.fileMapped = new File("/media/Fractal/fileMapped.tmp");
 
         FileOutputStream writer;
@@ -49,21 +47,22 @@ public class W13MemoryMapping implements Observer {
         koch.addObserver(this);
         Scanner scanner = new Scanner(System.in);
         System.out.print("Welk level gegenereerd worden?: ");
-        int level = scanner.nextInt();
+        level = scanner.nextInt();
         koch.setLevel(level);
         koch.generateBottomEdge();
         koch.generateLeftEdge();
         koch.generateRightEdge();
         //TimeStamp timeStamp = new TimeStamp();
-        writeFileMapped();
+        this.writeFileMapped();
+        this.readFileMapped();
     }
 
-    ArrayList<Edge> lijst = new ArrayList<>();
+    List<Edge> edges = new ArrayList<>();
 
     @Override
     public void update(Observable o, Object arg) {
         Edge e = (Edge) arg;
-        lijst.add(e);
+        edges.add(e);
     }
 
     public void writeFileMapped() throws IOException {
@@ -72,25 +71,47 @@ public class W13MemoryMapping implements Observer {
 
         long bufferSize = 8 * 1000;
         MappedByteBuffer mappedBB = fc.map(FileChannel.MapMode.READ_WRITE, 0, bufferSize);
-        
 
         int start = 0;
-        long counter = 1;
+        long counter = 0;
         long HUNDREDK = 100000;
-        long startT = System.currentTimeMillis();        
+        long startT = System.currentTimeMillis();
         long noOfMessage = HUNDREDK * 10 * 10;
-        
-        for (Edge edge : lijst){
+
+        for (Edge edge : edges) {
             mappedBB.putDouble(edge.X1);
-            mappedBB.putDouble(edge.X2);
             mappedBB.putDouble(edge.Y1);
+            mappedBB.putDouble(edge.X2);
             mappedBB.putDouble(edge.Y2);
             mappedBB.put(edge.color.getBytes());
-            mappedBB.putInt(edge.level);      
+            mappedBB.putInt(edge.level);
             counter++;
-            System.out.println("Total edges written: " + counter);
         }
+        System.out.println("Total edges written: " + counter);
+    }
+
+    public void readFileMapped() throws IOException {
+        edges.clear();
+        RandomAccessFile aFile = new RandomAccessFile(fileMapped.getAbsolutePath(), "r");
+        FileChannel inChannel = aFile.getChannel();
+        MappedByteBuffer buffer = inChannel.map(FileChannel.MapMode.READ_ONLY, 0, inChannel.size());
+        buffer.load();
+        String chars = "";
+        for (int i = 0; i < (int) (3 * Math.pow(4, level - 1)); i++) {
+            Edge edge = new Edge(buffer.getDouble(), buffer.getDouble(), buffer.getDouble(), buffer.getDouble(), "0x0000ffff", level);
+            edges.add(edge);
+        }
+        System.out.println("" + edges.size());
+        for (Edge edge : edges) {
+            System.out.println("-------");
+            System.out.println(edge.X1);
+            System.out.println(edge.Y1);
+            System.out.println(edge.X2);
+            System.out.println(edge.Y2);
+            System.out.println(edge.color);
+        }
+        buffer.clear(); // do something with the data and clear/compact it.
+        inChannel.close();
+        aFile.close();
     }
 }
-
-
