89966bdfd534f5799b59379d7c92a4c18c07b732
jsf3 <jsf3@jsf3-virtual-machine>
2015-12-08 15:01:36 +0100

    cleanup + timeStamp

--- W13MemoryMapping/src/w13memorymapping/W13MemoryMapping.java
+++ W13MemoryMapping/src/w13memorymapping/W13MemoryMapping.java
@@ -9,13 +9,9 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.RandomAccessFile;
-import java.nio.ByteBuffer;
-import java.nio.CharBuffer;
 import java.nio.MappedByteBuffer;
 import java.nio.channels.FileChannel;
-import java.nio.charset.Charset;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Observable;
 import java.util.Observer;
@@ -30,15 +26,16 @@ public class W13MemoryMapping implements Observer {
 
     /**
      * @param args the command line arguments
+     * @throws java.io.IOException
      */
     public static void main(String[] args) throws IOException {
-        // TODO code application logic here
-        W13MemoryMapping w13MemoryMapping = new W13MemoryMapping();
+        new W13MemoryMapping();
     }
 
-    private KochFractal koch;
-    private File fileMapped;
-    private int level;
+    private final KochFractal koch;
+    private final File fileMapped;
+    private final int level;
+    private TimeStamp timeStamp;
 
     public W13MemoryMapping() throws IOException {
         this.koch = new KochFractal();
@@ -55,9 +52,11 @@ public class W13MemoryMapping implements Observer {
         koch.generateBottomEdge();
         koch.generateLeftEdge();
         koch.generateRightEdge();
-        //TimeStamp timeStamp = new TimeStamp();
+
+        timeStamp = new TimeStamp();
         this.writeFileMapped();
         this.readFileMapped();
+        System.out.println(timeStamp.toString());
     }
 
     List<Edge> edges = new ArrayList<>();
@@ -69,17 +68,12 @@ public class W13MemoryMapping implements Observer {
     }
 
     public void writeFileMapped() throws IOException {
+        timeStamp.setBegin("begin writeFileMapped");
         fileMapped.delete();
         FileChannel fc = new RandomAccessFile(fileMapped, "rw").getChannel();
-
-        long bufferSize = 8 * 1000;
+        long bufferSize = 8 * 1000000;
         MappedByteBuffer mappedBB = fc.map(FileChannel.MapMode.READ_WRITE, 0, bufferSize);
-
-        int start = 0;
         long counter = 0;
-        long HUNDREDK = 100000;
-        long startT = System.currentTimeMillis();
-        long noOfMessage = HUNDREDK * 10 * 10;
 
         for (Edge edge : edges) {
             mappedBB.putDouble(edge.X1);
@@ -89,45 +83,44 @@ public class W13MemoryMapping implements Observer {
             mappedBB.putDouble(Color.valueOf(edge.color).getRed());
             mappedBB.putDouble(Color.valueOf(edge.color).getGreen());
             mappedBB.putDouble(Color.valueOf(edge.color).getBlue());
-            //System.out.println(Arrays.toString(edge.color.getBytes()));
-            //mappedBB.put(edge.color.getBytes());
             mappedBB.putInt(edge.level);
             counter++;
         }
+        timeStamp.setEnd("eind writeFileMapped");
         System.out.println("Total edges written: " + counter);
     }
 
     public void readFileMapped() throws IOException {
         edges.clear();
-        RandomAccessFile aFile = new RandomAccessFile(fileMapped.getAbsolutePath(), "r");
-        FileChannel inChannel = aFile.getChannel();
-        MappedByteBuffer buffer = inChannel.map(FileChannel.MapMode.READ_ONLY, 0, inChannel.size());
-        buffer.load();
-        for (int i = 0; i < (int) (3 * Math.pow(4, level - 1)); i++) {
-            double X1 = buffer.getDouble();
-            double Y1 = buffer.getDouble();
-            double X2 = buffer.getDouble();
-            double Y2 = buffer.getDouble();
-            String color = new Color(buffer.getDouble(), buffer.getDouble(), buffer.getDouble(), 1).toString();
-            //byte[] bytes = buffer.get
-            //String color = new String(buffer.get(bytes).array(), Charset.forName("UTF-8"));
-            int level = buffer.getInt();
-
-            Edge edge = new Edge(X1, Y1, X2, Y2, color, level);
-            edges.add(edge);
+        timeStamp.setBegin("begin readFileMapped");
+        try (RandomAccessFile aFile = new RandomAccessFile(fileMapped.getAbsolutePath(), "r"); FileChannel inChannel = aFile.getChannel()) {
+            MappedByteBuffer buffer = inChannel.map(FileChannel.MapMode.READ_ONLY, 0, inChannel.size());
+            buffer.load();
+            for (int i = 0; i < (int) (3 * Math.pow(4, level - 1)); i++) {
+                double X1 = buffer.getDouble();
+                double Y1 = buffer.getDouble();
+                double X2 = buffer.getDouble();
+                double Y2 = buffer.getDouble();
+                String color = new Color(buffer.getDouble(), buffer.getDouble(), buffer.getDouble(), 1).toString();
+                int level = buffer.getInt();
+                
+                //create edge
+                Edge edge = new Edge(X1, Y1, X2, Y2, color, level);
+                edges.add(edge);
+            }
+            timeStamp.setEnd("eind readFileMapped");
+            //print read edges
+            System.out.println("" + edges.size());
+            for (Edge edge : edges) {
+                System.out.println("-------");
+                System.out.println(edge.X1);
+                System.out.println(edge.Y1);
+                System.out.println(edge.X2);
+                System.out.println(edge.Y2);
+                System.out.println(edge.color);
+                System.out.println(edge.level);
+            }
+            buffer.clear(); // do something with the data and clear/compact it.
         }
-        System.out.println("" + edges.size());
-        for (Edge edge : edges) {
-            System.out.println("-------");
-            System.out.println(edge.X1);
-            System.out.println(edge.Y1);
-            System.out.println(edge.X2);
-            System.out.println(edge.Y2);
-            System.out.println(edge.color);
-            System.out.println(edge.level);
-        }
-        buffer.clear(); // do something with the data and clear/compact it.
-        inChannel.close();
-        aFile.close();
     }
 }
